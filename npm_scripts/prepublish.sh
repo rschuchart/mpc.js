a141efe028b6428a1c4438f22fca75e31caae045
Roderic Schuchart <rschuchart@evandor.de>
2015-10-16 13:49:44 +0200

    use the locally installed exorcist in the build script

--- npm_scripts/prepublish.sh
+++ npm_scripts/prepublish.sh
@@ -4,7 +4,7 @@
 ./node_modules/.bin/tsc --module commonjs --outDir lib/ src/node-mpc.ts src/browser-mpc.ts src/live-mpd-objects.ts
 
 # generate bower package
-./node_modules/.bin/browserify -s MPC -p tsify -d src/browser-mpc.ts src/live-mpd-objects.ts | exorcist bower/mpc.js.map > bower/mpc.js
+./node_modules/.bin/browserify -s MPC -p tsify -d src/browser-mpc.ts src/live-mpd-objects.ts | ./node_modules/.bin/exorcist bower/mpc.js.map > bower/mpc.js
 
 # generate documentation
 ./node_modules/.bin/typedoc -m commonjs --out doc/ --disableOutputCheck src/live-mpd-objects.ts src/node-mpc.ts src/browser-mpc.ts

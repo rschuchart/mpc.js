d31b272ee4b9d944fcd8ba1e824d29550c276b62
Roderic Schuchart <rschuchart@evandor.de>
2015-10-16 13:34:02 +0200

    generate documentation in npm lifecycle scripts

--- npm_scripts/prepublish.sh
+++ npm_scripts/prepublish.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# generate npm package
 ./node_modules/.bin/tsc --module commonjs --outDir lib/ src/node-mpc.ts src/browser-mpc.ts
 
+# generate bower package
 ./node_modules/.bin/browserify -s MPC -p tsify -d src/browser-mpc.ts | exorcist bower/mpc.js.map > bower/mpc.js
+
+# generate documentation
+./node_modules/.bin/typedoc -m commonjs --out doc/ --disableOutputCheck src/live-mpd-objects.ts src/node-mpc.ts src/browser-mpc.ts
--- npm_scripts/publish.sh
+++ npm_scripts/publish.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+# publish bower package
 cd bower
 git add -A .
 git commit -m "Release $npm_package_version"
 git tag "v$npm_package_version"
 git push
 git push --tags
+cd ..
+
+# publish documentation
+cd doc
+git add -A .
+git commit -m "Release $npm_package_version"
+git push
+cd ..
